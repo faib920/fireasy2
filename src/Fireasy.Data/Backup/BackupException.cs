4f56ea630327a922e4d39437b8fdc8ce1b99502c
faib <faib920@126.com>
2020-11-11 16:08:48 +0800

    add data.backup class files

---
+++ src/Fireasy.Data/Backup/BackupException.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Fireasy"
+//      email="faib920@126.com"
+//      qq="55570729">
+//   (c) Copyright Fireasy. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+using System;
+
+namespace Fireasy.Data.Backup
+{
+    /// <summary>
+    /// 数据库在备份或恢复过程中发生异常。无法继承此类。
+    /// </summary>
+    [Serializable]
+    public sealed class BackupException : Exception
+    {
+        /// <summary>
+        /// 初始化 <see cref="BackupException"/> 类的新实例。
+        /// </summary>
+        public BackupException()
+        {
+        }
+
+        /// <summary>
+        /// 初始化 <see cref="BackupException"/> 类的新实例。
+        /// </summary>
+        /// <param name="exception">导致当前异常的异常。</param>
+        public BackupException(Exception exception)
+            : base(SR.GetString(SRKind.FailInBackup, exception.Message), exception)
+        {
+        }
+
+        /// <summary>
+        /// 初始化 <see cref="BackupException"/> 类的新实例。
+        /// </summary>
+        /// <param name="message">解释异常原因的错误消息。</param>
+        /// <param name="exception">导致当前异常的异常</param>
+        public BackupException(string message, Exception exception)
+            : base(message, exception)
+        {
+        }
+    }
+}
---
+++ src/Fireasy.Data/Backup/IBackupProvider.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Fireasy"
+//      email="faib920@126.com"
+//      qq="55570729">
+//   (c) Copyright Fireasy. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Fireasy.Data.Provider;
+
+namespace Fireasy.Data.Backup
+{
+    /// <summary>
+    /// 提供对数据库备份与恢复的一组方法。
+    /// </summary>
+    public interface IBackupProvider : IProviderService
+    {
+        /// <summary>
+        /// 对指定的数据库进行备份。
+        /// </summary>
+        /// <param name="database">提供给当前插件的 <see cref="IDatabase"/> 对象。</param>
+        /// <param name="option">备份选项。</param>
+        void Backup(IDatabase database, BackupOption option);
+
+        /// <summary>
+        /// 使用指定的备份文件恢复数据库。
+        /// </summary>
+        /// <param name="database">提供给当前插件的 <see cref="IDatabase"/> 对象。</param>
+        /// <param name="option">备份选项。</param>
+        void Restore(IDatabase database, BackupOption option);
+    }
+}
