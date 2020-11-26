4a470674c207fa6e33ea8558169d16f8a03551c1
faib <faib920@126.com>
2020-11-26 11:04:56 +0800

    增加原来的几个方法

--- src/Fireasy.Web.Mvc/Rendering/ExtendHtmlExtensions.cs
+++ src/Fireasy.Web.Mvc/Rendering/ExtendHtmlExtensions.cs
@@ -32,5 +32,51 @@ namespace Fireasy.Web.Mvc.Rendering
             htmlHelper.Builder.AddCssClass(className);
             return htmlHelper;
         }
+
+        /// <summary>
+        /// 使当前的元素成为只读。
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <returns></returns>
+        public static ExtendHtmlString Readonly(this ExtendHtmlString htmlHelper)
+        {
+            return htmlHelper.AddAttribute("readonly", "readonly");
+        }
+
+        /// <summary>
+        /// 使当前的元素成为不可用。
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="className"></param>
+        /// <returns></returns>
+        public static ExtendHtmlString Disable(this ExtendHtmlString htmlHelper)
+        {
+            return htmlHelper.AddAttribute("disabled", "disabled");
+        }
+
+        /// <summary>
+        /// 添加属性。
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="attributeName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ExtendHtmlString AddAttribute(this ExtendHtmlString htmlHelper, string attributeName, string value)
+        {
+            htmlHelper.Builder.MergeAttribute(attributeName, value);
+            return htmlHelper;
+        }
+
+        /// <summary>
+        /// 移除属性。
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="attributeName"></param>
+        /// <returns></returns>
+        public static ExtendHtmlString RemoveAttribute(this ExtendHtmlString htmlHelper, string attributeName)
+        {
+            htmlHelper.Builder.Attributes.Remove(attributeName);
+            return htmlHelper;
+        }
     }
 }
