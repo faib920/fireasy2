66a0ebad5ebcd6fef4b827a8a44641b90e78a5eb
faib <faib920@126.com>
2020-11-27 00:16:18 +0800

    增加IsNull函数

--- src/Fireasy.Data.Entity/Linq/SqlFunc.cs
+++ src/Fireasy.Data.Entity/Linq/SqlFunc.cs
@@ -16,34 +16,73 @@ namespace Fireasy.Data.Entity.Linq
     /// </summary>
     public static class SqlFunc
     {
+        /// <summary>
+        /// 取记录数。
+        /// </summary>
+        /// <returns></returns>
         [MethodCallBind(typeof(CountFunctionBinder))]
         public static int Count()
         {
             return 0;
         }
 
+        /// <summary>
+        /// 取字段的总和。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
         [MethodCallBind(typeof(SumFunctionBinder))]
         public static T Sum<T>(T value)
         {
             return value;
         }
 
+        /// <summary>
+        /// 取字段的平均值。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
         [MethodCallBind(typeof(AverageFunctionBinder))]
         public static T Average<T>(T value)
         {
             return value;
         }
 
+        /// <summary>
+        /// 取字段的最大值。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
         [MethodCallBind(typeof(MaxFunctionBinder))]
         public static T Max<T>(T value)
         {
             return value;
         }
 
+        /// <summary>
+        /// 取字段的最小值。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
         [MethodCallBind(typeof(MinFunctionBinder))]
         public static T Min<T>(T value)
         {
             return value;
+        }
+
+        /// <summary>
+        /// 判断字段是否为 NULL。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [MethodCallBind(typeof(IsNullFunctionBinder))]
+        public static bool IsNull(object value)
+        {
+            return true;
         }
 
         private abstract class AggregateFunctionBinder : IMethodCallBinder
@@ -99,6 +138,15 @@ namespace Fireasy.Data.Entity.Linq
             public MinFunctionBinder()
                 : base(AggregateType.Min)
             {
+            }
+        }
+
+        private class IsNullFunctionBinder : IMethodCallBinder
+        {
+            public Expression Bind(MethodCallBindContext context)
+            {
+                var expression = context.Visitor.Visit(context.Expression.Arguments[0]);
+                return new IsNullExpression(expression);
             }
         }
     }
